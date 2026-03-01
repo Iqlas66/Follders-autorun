0e3442de33afd0978b42fa0c45c3a772e0b9c0cb
iqlas <imiqlas@gmail.com>
2026-03-01 18:44:55 +0100

    Added function for getting explorer paths

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import win32com.client as win
+def get_open_explorer_paths():
+    shell = win.Dispatch("Shell.Application")
+    paths = []
+    for window in shell.Windows():
+        try:
+            path = window.Document.Folder.Self.Path
+            paths.append(path)
+        except:
+            pass
+    return paths
